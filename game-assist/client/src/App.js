5da637a8619069e3b6349d8bf11ffbc42757b770
Brittany <brittanygillespie31@gmail.com>
2018-06-25 19:29:15 -0400

    adding pages

--- game-assist/client/src/App.js
+++ game-assist/client/src/App.js
@@ -1,5 +1,9 @@
 import React from('react');
+import { Route } from 'react-router-dom':
 
-const App = () => <div> Hello World </div>
+const App = () => 
+<div>
+    <Route path="/" exact component={HomePage} />
+</div>
 
 export default App;
