077e7325522190ad5538799a033ac862612b7f94
Brittany <brittanygillespie31@gmail.com>
2018-06-26 17:50:32 -0400

    adding home and login page skeltons

--- game-assist/client/src/App.js
+++ game-assist/client/src/App.js
@@ -1,9 +1,13 @@
 import React from('react');
-import { Route } from 'react-router-dom':
+import { Route } from 'react-router-dom';
+import HomePage from "./components/Pages/HomePage";
+import HomePage from "./components/Pages/LoginPage";
 
 const App = () => 
 <div>
     <Route path="/" exact component={HomePage} />
+    <Route path="/" exact component={LoginPage} />
+    
 </div>
 
 export default App;
