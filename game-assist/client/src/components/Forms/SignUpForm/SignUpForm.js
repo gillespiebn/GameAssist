32fd6e95aaf987d17918d03fd8d142832e1d5601
Brittany <brittanygillespie31@gmail.com>
2018-06-26 17:59:31 -0400

    adding login and signup forms skeletons?

---
+++ game-assist/client/src/components/Forms/SignUpForm/SignUpForm.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class SignUpForm extends React.Component {
+    state={
+
+    }
+
+    render() {
+        return(
+            <div></div>
+        );
+    }
+}
+
+export default SignUpForm;
--- game-assist/client/src/components/Pages/LoginPage.js
+++ game-assist/client/src/components/Pages/LoginPage.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import LoginForm from "../forms/LoginForm";
 
 const LoginPage = (props) => (
     <div>
         <h1>Login</h1>
-        
+        <LoginForm />
     </div>
 );
 
