d41e1077bba07b6ec67ae19801264b31c84988e3
Brittany <brittanygillespie31@gmail.com>
2018-07-07 16:52:26 -0400

    added header and footer skeletons

---
+++ game-assist/client/src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+  return(
+    <footer className="footer">
+            <span className="footer-text">&copy; Brittany Gillespie 2018</span>
+    </footer>
+  )
+}
+
+export default Footer;
---
+++ game-assist/client/src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import "./Header.css";
+
+
+const Header = () => (
+    <header>
+        <h1> GameAssist </h1>
+    </header>
+  )
+
+export default Header;
