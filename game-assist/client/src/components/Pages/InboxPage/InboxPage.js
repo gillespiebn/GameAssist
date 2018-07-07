888efc9596733ccfd84149ebafb3bcacfc9980f0
Brittany <brittanygillespie31@gmail.com>
2018-07-07 16:55:51 -0400

    Adding message and inbox pages and corresponding files

---
+++ game-assist/client/src/components/Pages/InboxPage/InboxPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+
+const InboxPage = (props) => (
+    <div>
+        <h1> Inbox </h1>
+        
+    </div>
+);
+
+export default InboxPage;
