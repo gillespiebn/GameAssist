fd10aa5a4d44c7aa75607cb30696740d54bc14d1
Brittany <brittanygillespie31@gmail.com>
2018-07-22 21:49:15 -0400

    making sure everything is up to date

--- game-assist/client/src/index.js
+++ game-assist/client/src/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDom.render(<App />, document.getElementById('root'));
+ReactDom.render(
+<BrowserRouter>
+    <App />
+</BrowserRouter>,
+ document.getElementById('root'));
 registerServiceWorker();
